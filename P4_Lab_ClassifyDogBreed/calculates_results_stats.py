ff480c205149193cd21adec37b1b144ed1914e61
ZenRay <jake2011ren@gmail.com>
2018-12-25 00:00:08 +0800

    refactor($Project4lab): Change the function calculates_results_stats code

--- P4_Lab_ClassifyDogBreed/calculates_results_stats.py
+++ P4_Lab_ClassifyDogBreed/calculates_results_stats.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats.py
 #                                                                             
-# PROGRAMMER:
+# PROGRAMMER: RayZen
 # DATE CREATED:                                  
-# REVISED DATE: 
+# REVISED DATE: 24/12/18
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
 #          architecture to classify the images. This function will use the 
@@ -70,4 +70,60 @@ def calculates_results_stats(results_dic):
     """        
     # Replace None with the results_stats_dic dictionary that you created with 
     # this function 
-    return None
+    results_stats_dic = dict()
+    
+    # init the dict key
+    results_stats_dic["n_dogs_img"] = 0
+    results_stats_dic["n_match"] = 0
+    results_stats_dic["n_correct_dogs"] = 0
+    results_stats_dic["n_correct_notdogs"] = 0
+    results_stats_dic["n_correct_breed"] = 0
+
+    # loop the results_dic
+    for key in results_dic:
+        # Labels Match Exactly
+        if results_dic[key][2] == 1:
+            results_stats_dic["n_match"] += 1
+        # Pet Image Label is a Dog AND Labels match- counts Correct Breed
+        if results_dic[key][3] == 1 and results_dic[key][2] == 1:
+            results_stats_dic["n_correct_breed"] += 1
+        
+        # Pet Image Label is a Dog - counts number of dog images
+        if results_dic[key][3] == 1:
+            results_stats_dic['n_dogs_img'] += 1
+        # Classifier classifies image as Dog (& pet image is a dog)
+            # counts number of correct dog classifications
+            if results_dic[key][4] == 1:
+                results_stats_dic['n_correct_dogs'] += 1
+        # Pet Image Label is NOT a Dog
+        else:
+            if results_dic[key][4] == 1:
+                results_stats_dic["n_correct_notdogs"] += 1
+    # calculates number of total images
+    results_stats_dic['n_images'] = len(results_dic)
+
+    # calculates number of not-a-dog images using - images & dog images counts
+    results_stats_dic['n_notdogs_img'] = (results_stats_dic['n_images'] - 
+                                      results_stats_dic['n_dogs_img']) 
+    
+    # Calculates % correct for matches
+    results_stats_dic['pct_match'] = (results_stats_dic["n_match"] / 
+                                        results_stats_dic["n_images"])
+
+    # Calculates % correct dogs
+    results_stats_dic['pct_correct_dogs'] = \
+        (results_stats_dic["n_correct_dogs"] / results_stats_dic["n_dogs_img"])
+
+    # Calculates % correct breed of dog
+    results_stats_dic['pct_correct_breed'] = \
+        (results_stats_dic["n_correct_breed"] / results_stats_dic["n_dogs_img"])
+
+    # Calculates % correct not-a-dog images
+    # Uses conditional statement for when no 'not a dog' images were submitted 
+    if results_stats_dic['n_notdogs_img'] > 0:
+        results_stats_dic['pct_correct_notdogs'] = (results_stats_dic['n_correct_notdogs'] /
+                                                results_stats_dic['n_notdogs_img'])*100.0
+    else:
+        results_stats_dic['pct_correct_notdogs'] = 0.0
+    
+    return results_stats_dic
